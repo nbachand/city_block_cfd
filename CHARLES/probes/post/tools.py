3428cf65a776d30fb4764919342fee24e4872963
nbachand <ngbachand@gmail.com>
2022-07-19 14:10:17 -0700

    seperating testing script from tools

--- CHARLES/probes/post/tools.py
+++ CHARLES/probes/post/tools.py
@@ -1,11 +1,13 @@
 import pandas as pd
 import glob
-import time
 
-def read_pcd(filename):
+def read_probes(filename):
     return pd.read_csv(filename, delim_whitespace=True)
+
+def read_locations(filename):
+    return pd.read_csv(filename, delim_whitespace=True, skiprows=1, names=['x','y','z'])
             
-class MyProbeDict(dict):
+class MyLazyDict(dict):
     '''
     Create a lazy dictionary by modifying the __getitem__ attribute. New dictionary dynamically reads in data as it is accessed,
     and memorizes data once it has been read in.
@@ -13,7 +15,7 @@ class MyProbeDict(dict):
     def __getitem__(self, item):
         value=dict.__getitem__(self, item) # retrieve the current dictionary value
         if not isinstance(value, pd.core.frame.DataFrame): # check if data has been read in
-            # print('reading in probe data')
+            # print('reading in data')
             function, arg = value # retrieve data reading function and data path
             value = function(arg) # read in the data, and assign it to the dict value
             dict.__setitem__(self, item, value) # reset the dictionary value to the data
@@ -25,6 +27,7 @@ class Probes:
 
         my_dict= {}
         path_generator = glob.iglob(f'{directory}/*.pcd') # create a generator to iterate over probe paths
+        self.probe_names = []
 
         for path in path_generator:
 
@@ -33,22 +36,21 @@ class Probes:
             probe_name, probe_number, _ = probe_info[:]
             probe_number = int(probe_number)
 
-            # create a dictionary for each probe name if it does not exist
             if probe_name not in my_dict.keys():
-                my_dict[probe_name] = {}
+                my_dict[probe_name] = {} # create a dictionary for each probe name if it does not exist
+                self.probe_names.append(probe_name)
 
-            my_dict[probe_name][probe_number] = (read_pcd, path) # store the pcd path and pcd reader function
+            my_dict[probe_name][probe_number] = (read_probes, path) # store the pcd path and pcd reader function
 
         #iterate through the upper data dict
         for probe_name, name_dict in my_dict.items():
-            my_dict[probe_name] = MyProbeDict(name_dict) # modify the getter or the lower-level dicts to lazily read in data
+            my_dict[probe_name] = MyLazyDict(name_dict) # modify the getter or the lower-level dicts to lazily read in data
         self.data = my_dict
 
+    def get_locations(self, dir_locations):
+        locations = {}
+        for probe_name in self.probe_names:
+            location_path = f"{dir_locations}/{probe_name}.txt"
+            locations[probe_name] = (read_locations, location_path)
+        self.locations = MyLazyDict(locations)
 
-st = time.time()
-test = Probes('./CHARLES/probes/probesOut')
-et = time.time()
-
-print(et-st)
-print(test.data['x_1over6'][79743]['comp(u,0)'])
-print(test.data['x_1over6'][79742]['comp(u,0)'])
--- CHARLES/probes/post_processing.py
+++ CHARLES/probes/post_processing.py
@@ -1,3 +1,21 @@
+#%%
 import post.tools as tools
+import time
+# import pathlib
 
 #%%
+# working_dir = pathlib.Path().absolute()
+working_dir = '/home/nbachand/Documents/Cascade/city_block'
+
+st = time.time()
+test = tools.Probes(f'{working_dir}/CHARLES/probes/probesOut')
+test.get_locations(f'{working_dir}/CHARLES/probes/locations')
+et = time.time()
+
+print(et-st)
+print(test.data['x_1over6'][79743]['comp(u,0)'])
+print(test.data['x_1over6'][79743]['comp(u,0)'])
+
+print(test.locations['x_1over6'])
+print(test.locations['x_1over6'])
+# %%
