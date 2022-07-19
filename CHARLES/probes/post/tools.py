eaaa921ea9bc2097506db1d50bdf7a7f18040087
nbachand <ngbachand@gmail.com>
2022-07-19 12:16:38 -0700

    changing probe data structure to multi-level dict

--- CHARLES/probes/post/tools.py
+++ CHARLES/probes/post/tools.py
@@ -3,13 +3,13 @@ import glob
 import time
 
 def read_pcd(filename):
-    print(f'type: {type(pd.read_csv(filename, delim_whitespace=True))}')
     return pd.read_csv(filename, delim_whitespace=True)
             
 class MyProbeDict(dict):
     def __getitem__(self, item):
         value=dict.__getitem__(self, item)
         if not isinstance(value, pd.core.frame.DataFrame):
+            # print('reading in probe data')
             function, arg = value
             value = function(arg)
             dict.__setitem__(self, item, value)
@@ -18,34 +18,31 @@ class MyProbeDict(dict):
 
 class Probes:
     def __init__(self, directory):
-        self.data = None
 
         my_dict= {}
-        path_generator = glob.iglob(f'{directory}/*.pcd')
-        i = 0
+        path_generator = glob.iglob(f'{directory}/*.pcd') # retrieve paths for all probes
+
         for path in path_generator:
-            i+=1
-            if i > 10000:
-                break
 
             file_name = path.split('/')[-1]
             probe_info = file_name.split('.')
             probe_name, probe_number, _ = probe_info[:]
             probe_number = int(probe_number)
-            probe = f'{probe_name}{probe_number}'
 
-            # if probe_number in self.data[probe_name]:
-            #     print("WARNING: duplicate probes")
-            my_dict[probe] = (read_pcd, path)
-        self.data = MyProbeDict(my_dict)
+            if probe_name not in my_dict.keys():
+                my_dict[probe_name] = {}
+
+            my_dict[probe_name][probe_number] = (read_pcd, path)
+
+        for probe_name, name_dict in my_dict.items():
+            my_dict[probe_name] = MyProbeDict(name_dict)
+        self.data = my_dict
 
 
 st = time.time()
-test = Probes('./probesOut')
-# print(test.data)
+test = Probes('./CHARLES/probes/probesOut')
 et = time.time()
 
 print(et-st)
-probe = 'x_1over6'+'88300'
-print(test.data[probe])
-print(test.data[probe])
+print(test.data['x_1over6'][79743]['comp(u,0)'])
+print(test.data['x_1over6'][79742]['comp(u,0)'])
