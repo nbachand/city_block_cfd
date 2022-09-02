6e882bb22927d5c10709d583de7eac3c49acb329
nbachand <ngbachand@gmail.com>
2022-09-02 12:29:40 -0700

    creating seperate pyCascade package

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='pyCascade',
+    packages=find_packages(include=['pyCascade']),
+    version='0.1.0',
+    description='python library to complement Cascade simulation tools',
+    author='Nicholas Bachand',
+    license='MIT',
+    install_requires=['glob', 'numpy', 'pandas', 'matplotlib', 'pandarallel', 'time'],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest==4.4.1'],
+    test_suite='tests',
+)
