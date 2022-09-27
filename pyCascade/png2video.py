429c165da103e2fd9eb7e718eca91ddbdbb223d2
nbachand <ngbachand@gmail.com>
2022-09-26 17:31:00 -0700

    video output defaults to namefmt instead of video

--- pyCascade/png2video.py
+++ pyCascade/png2video.py
@@ -7,6 +7,7 @@
 # --output      Name of video file to create
 # --mirror      Include to mirror images horizontally
 
+from pyCascade import utils
 import argparse
 import cv2
 import os
@@ -17,10 +18,19 @@ def main():
     parser.add_argument('--namefmt', type=str, required=True, help='Format of naming, e.g. if the images look like `pavg_Xslice.00004000.png`, enter `pavg_Xslice.`')
     parser.add_argument('--dt', type=float, required=True, help='Timestep of simulation')
     parser.add_argument('--subsample', type=int, default=1, help='Frequency with which to subsample the frames')
-    parser.add_argument('--output', type=str, default='video.avi', help='Name of video file to create')
+    parser.add_argument('--output', type=str, default=None, help='Name of video file to create, defaults to namefmt')
     parser.add_argument('--mirror', help='Include to mirror images horizontally', action='store_true')
     args = parser.parse_args()
 
+    if args.output == None:
+        args.output = args.namefmt
+        if args.output[-1] != ".":
+            args.output = f"{args.output}."
+        args.output = f"{args.output}avi"
+        
+
+    st = utils.start_timer("creating video...")
+    
     images = [img for img in os.listdir(args.directory) if img.startswith(args.namefmt) and img.endswith(".png")]
     images.sort()
     frame = cv2.imread(os.path.join(args.directory, images[0]))
@@ -36,6 +46,8 @@ def main():
 
     cv2.destroyAllWindows()
     video.release()
+
+    utils.end_timer(st, "creating video")
 
 if __name__ == "__main__":
     main()
