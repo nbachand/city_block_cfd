1a8077de11ff5b937c701328e717c3909a2099e3
Nicholas <ngbachand@gmail.com>
2023-05-11 18:59:42 -0700

    finished option to copy location text files over

--- CHARLES/setup_run_folders.sh
+++ CHARLES/setup_run_folders.sh
@@ -21,13 +21,11 @@ mkdir $LOCAL_FOLDER/"data"
 mkdir $LOCAL_FOLDER/"Images"
 mkdir $LOCAL_FOLDER/"probes"
 mkdir $LOCAL_FOLDER/"probes/probesOut"
+mkdir $LOCAL_FOLDER"/probes/locations"
 
-read -p "Optional: Enter run to copy probe locations from [None]: " R_locations
-R_locations=${R_locations:"none"}
-echo $R_locations # (optional) move to a new line
-if [${R_locations} != "none"];
-then
-    cp -r "../$R_locations/probes/locations ./probes"
-else
-    mkdir $LOCAL_FOLDER/"probes/locations"
+read -p "Optional: Enter run to copy probe locations from [skip]:" R_locations
+R_locations="${R_locations:=skip}"
+echo "$R_locations" # (optional) move to a new line
+if [ "$R_locations" != "skip" ]; then
+    cp -r ./$R_locations/probes/locations/* $LOCAL_FOLDER"/probes/locations"
 fi
