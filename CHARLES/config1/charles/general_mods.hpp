209b97d6d2c41144773fefc8b21982a532264441
Nicholas <ngbachand@gmail.com>
2023-05-12 17:51:47 -0700

    breaking out modifications into general_mods.hpp

--- CHARLES/config1/charles/general_mods.hpp
+++ CHARLES/config1/charles/general_mods.hpp
@@ -7,6 +7,12 @@
 #include "NonpremixedSolver.hpp"
 #include "BasicPostpro.hpp"
 
+const double uStar = 0.4958;
+const double z0 = 0.366;
+const double disp = 6.66;
+const double domain_height = 192;
+const double vK_const = 0.41;
+
 //==================================================================================
 // following solver types are defined below
 //      -- IdealGasSolver : non-reacting, compressible solver
@@ -61,123 +67,11 @@
 //
 //==================================================================================
 
-//===============================
-// IdealGasSolver
-//===============================
-
-class MyIdealGasSolver : public IdealGasSolver {
-public:
-
-  /*
-  double (* vort)[3];         // = curl(u);
-  double * vort_mag;          // = |curl(u)|
-  */
-
-  MyIdealGasSolver() {
-
-    /*
-    vort     = NULL;  registerCvData( vort, "vort", READWRITE_DATA);
-    vort_mag = NULL;  registerCvData( vort_mag, "vort_mag", NO_READWRITE_DATA);
-    */
-  }
-
-  void initData() {
-
-    IdealGasSolver::initData();
-
-    /*
-    assert( vort == NULL);     vort     = new double[ncv_g2][3];  // include vort calculation in ghosts...
-    assert( vort_mag == NULL); vort_mag = new double[ncv];        // no ghosts ...
-    */
-
-  }
-
-  ~MyIdealGasSolver() {
-
-    /*
-    DELETE(vort);
-    DELETE(vort_mag);
-    */
-
-  }
-
-  void initialHook() {}
-
-  void temporalHook() {
-
-    /*
-    if ( step % check_interval == 0) {
-
-      if ( mpi_rank == 0 )
-        cout << " > computing vorticity ... " << endl;
-
-      for (int icv = 0; icv < ncv; ++icv) {
-
-        vort[icv][0]  = dudx[icv][2][1] - dudx[icv][1][2];
-        vort[icv][1]  = dudx[icv][0][2] - dudx[icv][2][0];
-        vort[icv][2]  = dudx[icv][1][0] - dudx[icv][0][1];
-
-        vort_mag[icv] = MAG(vort[icv]);
-
-      }
-
-      updateCv2Data(vort, REPLACE_ROTATE_DATA);
-
-    }
-    */
-
-  }
-
-  void finalHook() {}
-
-  IdealGasBc* initHookBc(BfZone* p) {
-    CERR(" > user must supply a hook boundary condition for zone: " << p->getName());
-  }
-
-  void addSourceHook(IdealGasRhs* rhs, const double time, const int rk_stage) {}
-
-};
-
-//===============================
-// PremixedSolver
-//===============================
-
-class MyPremixedSolver : public PremixedSolver {
-public:
-
-  MyPremixedSolver() {}
-
-  void initData() {
-
-    PremixedSolver::initData();
-
-  }
-
-  ~MyPremixedSolver() {}
-
-  void initialHook() {}
-
-  void temporalHook() {}
-
-  void finalHook() {}
-
-  PremixedBc* initHookBc(BfZone* p) {
-    CERR(" > user must supply a hook boundary condition for zone: " << p->getName());
-  }
-
-  void addSourceHook(PremixedRhs* rhs, const double time, const int rk_stage) {}
-
-};
 
 //===============================
 // HelmholtzSolver
 //===============================
 
-const double uStar = 0.4958;
-const double z0 = 0.366;
-const double disp = 6.66;
-const double domain_height = 144;
-const double vK_const = 0.41;
 
 class MyHelmholtzSolver : public HelmholtzSolver {
 public:
@@ -212,9 +106,10 @@ public:
         y_scaled = max(1.0, y_scaled);
         const double u_loglaw = (uStar/vK_const)*log(y_scaled);
 
-        u[icv][0] = 2*(u_loglaw - u_bulk*(absy/domain_height));
-        // u[icv][0] = 0.001;
-        u[icv][1] = 0.001;
+        const double u_parr = 2*(u_loglaw - u_bulk*(absy/domain_height));
+
+        u[icv][0] = u_parr*cos(theta_wind);
+        u[icv][1] = u_parr*sin(theta_wind);
         u[icv][2] = 0.001;
           
         // transport_scalar_vec[0][icv]=-0.1*absy;
@@ -253,7 +148,9 @@ public:
     const double factor = 1;
 
     FOR_ICV {
-      rhs[icv][0] += factor*vol_cv[icv]*pow(uStar,2)/domain_height;
+      const double mom_source = factor*vol_cv[icv]*pow(uStar,2)/domain_height;
+      rhs[icv][0] += sin(theta_wind)*mom_source;
+      rhs[icv][1] += cos(theta_wind)*mom_source;
     }
 
 //     if ( mpi_rank == 0 ) 
@@ -320,165 +217,3 @@ public:
   void addSourceHook(IdealGasRhs* rhs, const double time, const int rk_stage) {}
 
 };
-
-//===============================
-// postpro
-//===============================
-
-class MyBasicPostpro : public BasicPostpro {
-public:
-
-  MyBasicPostpro() {}
-
-  void initData() {
-
-    BasicPostpro::initData();
-
-  }
-
-  ~MyBasicPostpro() {}
-
-  void initialHook() {}
-
-  void temporalHook() {}
-
-  void finalHook() {}
-
-};
-
-
-int main(int argc, char* argv[]) {
-
-  try {
-
-    CTI_Init(argc,argv,"charles.in");
-
-    {
-
-      const bool b_post = checkParam("POST");
-
-      if (Param * param = getParam("EOS")) {
-        const string eos = param->getString();
-        if ( eos == "IDEAL_GAS") {
-
-          MyIdealGasSolver solver;
-
-          if (b_post) {
-
-            solver.initMin();
-            solver.runPost();
-
-          } else {
-
-            solver.init();
-            solver.run();
-          }
-        }
-        else if (eos == "IDEAL_GAS_LSP") {
-
-          MyIdealGasSolverWithLsp solver;
-
-          if (b_post) {
-
-            solver.initMin();
-            solver.runPost();
-
-          } else {
-
-            solver.init();
-            solver.run();
-          }
-        }
-        else if ((eos == "PREMIXED_FPV") || (eos == "PREMIXED")) {
-
-          MyPremixedSolver solver;
-
-          if ( b_post) {
-
-            solver.initMin();
-            solver.runPost();
-
-          } else {
-
-            solver.init();
-            solver.run();
-          }
-        }
-        else if ((eos == "NONPREMIXED_FPV") || (eos == "NONPREMIXED") || (eos == "NON-PREMIXED") || (eos == "NON-PREMIXED_FPV")) {
-
-          MyNonpremixedSolver solver;
-
-          if ( b_post) {
-
-            solver.initMin();
-            solver.runPost();
-
-          } else {
-
-            solver.init();
-            solver.run();
-
-          }
-        }
-        else if ( eos == "HELMHOLTZ") {
-
-          MyHelmholtzSolver solver;
-
-          if ( b_post) {
-
-            solver.initMin();
-            solver.runPost();
-
-          } else {
-
-            solver.init();
-            solver.run();
-          }
-        }
-        else {
-          CERR("unrecognized EOS: " << eos << ", possible choices are \n" <<
-               "EOS IDEAL_GAS\n" <<
-               "EOS PREMIXED_FPV\n" <<
-               "EOS NONPREMIXED_FPV\n" <<
-               "EOS IDEAL_GAS_LSP");
-        }
-      }
-      else {
-
-        if ( b_post) {
-
-          // if there is no eos object provided but we are in a post
-          // mode then we need to just start a stripped down staticsolver
-
-          MyBasicPostpro solver;
-          solver.initMin();
-          solver.runPost();
-
-        } else {
-
-          CERR("must specify EOS or POST. Possible choices for EOS are \n" <<
-               "EOS IDEAL_GAS\n" <<
-               "EOS PREMIXED_FPV\n" <<
-               "EOS NONPREMIXED_FPV\n" <<
-               "EOS IDEAL_GAS_LSP");
-        }
-      }
-    }
-
-    CTI_Finalize();
-  }
-  catch (int e) {
-    if (e >= 0) {
-      CTI_Finalize();
-    }
-    else {
-      CTI_Abort();
-    }
-  }
-  catch(...) {
-    CTI_Abort();
-  }
-
-  return 0;
-
-}
