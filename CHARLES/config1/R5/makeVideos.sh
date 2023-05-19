e1e308fcabbbd295afaa40371757627f1097def7
Nicholas Bachand <ngbachand@gmail.com>
2023-05-18 23:40:41 -0700

    post processing

---
+++ CHARLES/config1/R5/makeVideos.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+source "../SherlockVidoesHelper.sh"
+
+python $PYCASCADE_PATH/png2video.py --directory $IMAGE_DIR --namefmt "u_y5". --dt .01 --subsample 1
+
+python $PYCASCADE_PATH/png2video.py --directory $IMAGE_DIR --namefmt "u_y10". --dt .01 --subsample 1
+
+python $PYCASCADE_PATH/png2video.py --directory $IMAGE_DIR --namefmt "u_z265". --dt .01 --subsample 1
+
