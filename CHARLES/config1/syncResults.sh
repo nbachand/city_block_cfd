15b2ea2941d23da83ee4823329c50e703e244eff
Nicholas <ngbachand@gmail.com>
2023-05-19 10:21:25 -0700

    processign flow rates

--- CHARLES/config1/syncResults.sh
+++ CHARLES/config1/syncResults.sh
@@ -5,4 +5,4 @@ mkdir ./Images
 
 rsync -P -r "sherlock:/home/users/nbachand/Cascade/city_block_cfd/CHARLES/config1/$LOCAL_FOLDER/plots" ./
 rsync -P -r "sherlock:/home/users/nbachand/Cascade/city_block_cfd/CHARLES/config1/$LOCAL_FOLDER/tables" ./
-rsync -P "sherlock:/scratch/users/nbachand/Cascade/city_block_cfd/CHARLES/config1/$LOCAL_FOLDER/Images/u_y5*" ./Images/
+# rsync -P "sherlock:/scratch/users/nbachand/Cascade/city_block_cfd/CHARLES/config1/$LOCAL_FOLDER/Images/u_y5*" ./Images/
