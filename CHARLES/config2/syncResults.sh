312baf590a2bd0cb26a161d5d2fcb41842374748
Nicholas <ngbachand@gmail.com>
2023-09-25 11:53:50 -0700

    using new helper scripts

--- CHARLES/config2/syncResults.sh
+++ CHARLES/config2/syncResults.sh
@@ -3,8 +3,8 @@
 LOCAL_FOLDER=$(basename "`pwd`")
 mkdir ./Images
 
-rsync -P -az "sherlock:/home/users/nbachand/Cascade/city_block_cfd/CHARLES/config2/$LOCAL_FOLDER/plots" ./
-rsync -P -az "sherlock:/home/users/nbachand/Cascade/city_block_cfd/CHARLES/config2/$LOCAL_FOLDER/tables" ./
-rsync -P -az "sherlock:/scratch/users/nbachand/Cascade/city_block_cfd/CHARLES/config2/$LOCAL_FOLDER/Videos" ./
+sherlockHomeSync plots
+sherlockHomeSync tables
+sherlockScratchSync Videos
 
-# rsync -P -z "sherlock:/scratch/users/nbachand/Cascade/city_block_cfd/CHARLES/config2/$LOCAL_FOLDER/Images/u_y5*" ./Images/
+# sherlockScratchSync Images
