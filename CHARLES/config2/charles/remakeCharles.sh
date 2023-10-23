8507f43a212c399fb6f7256994dabdc90ce75505
Nicholas Bachand <ngbachand@gmail.com>
2023-10-23 15:08:14 -0700

    Finished script that makes charles in all subfolders

--- CHARLES/config2/charles/remakeCharles.sh
+++ CHARLES/config2/charles/remakeCharles.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-find . -maxdepth 1 -mindepth 1 -not -path '*/.*' -type d -exec sbatch Make.slurm
+find . -mindepth 1 -maxdepth 3 -name '*Make.slurm' -type f -execdir pwd \;;
+find . -mindepth 1 -maxdepth 3 -name '*slurm-*' -type f -execdir rm {} \;;
+find . -mindepth 1 -maxdepth 3 -name '*Make.slurm' -type f -execdir sbatch {} \;;
