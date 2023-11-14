196a91b059183af91cf6c4c80d692b0e7656db7e
Nicholas <ngbachand@gmail.com>
2023-11-14 13:51:43 -0800

    breaking bharles mods out of configs

--- CHARLES/config2/charles/45_deg_4mps/charles.cpp
+++ CHARLES/config2/charles/45_deg_4mps/charles.cpp
@@ -4,4 +4,6 @@ const double u_scaling = 6.3;
 const double theta_wind = 45 * M_PI/180;
 const double momentum_scaling_factor = 1.0;
 
-#include "../general_mods.hpp"
+#include "../constants.hpp"
+#include "../../../charles_mods/general_mods.hpp"
+
