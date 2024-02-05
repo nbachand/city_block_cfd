d30a3fc5d1b0367506fab3b6708ee7b513e48c42
Nicholas Bachand <ngbachand@gmail.com>
2024-02-05 10:38:21 -0800

    post processing

--- CHARLES/config2/SherlockVidoesHelper.sh
+++ CHARLES/config2/SherlockVidoesHelper.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 module load system libglvnd/1.2.0
 source ~/.bash_profile
+conda activate pycascade
 
 PARENT_DIR="Cascade/city_block_cfd/CHARLES/config2"
 LOCAL_FOLDER=$(basename "`pwd`")
 
-IMAGE_DIR=$SCRATCH/$PARENT_DIR/$LOCAL_FOLDER/Images
+IMAGE_DIR=$SCRATCH/$PARENT_DIR/$LOCAL_FOLDER/Images/images_rescaled/
 echo "Getting images from $IMAGE_DIR"
 
 cd $SCRATCH/$PARENT_DIR/$LOCAL_FOLDER
