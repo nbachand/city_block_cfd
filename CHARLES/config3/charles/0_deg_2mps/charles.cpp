7657bbc3df6c6f402b94e1ca4614e3f568a678e9
Nicholas Bachand <ngbachand@gmail.com>
2024-02-08 10:41:07 -0800

    fixing initialization for 2mps wind case, seemed minor

--- CHARLES/config3/charles/0_deg_2mps/charles.cpp
+++ CHARLES/config3/charles/0_deg_2mps/charles.cpp
@@ -1,6 +1,6 @@
 #include <cmath>
 #include "../constants.hpp"
-const double uStar = uStar_ref_4mps;
+const double uStar = uStar_ref_4mps / 2;
 const double u_scaling = u_ref_4mps / 2;
 const double theta_wind = 0 * M_PI/180;
 const double momentum_scaling_factor = 1.0;
