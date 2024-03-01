cc1b68999c55774fbf06d766accdd273fbf4f4fb
Nicholas Bachand <ngbachand@gmail.com>
2024-03-01 13:57:23 -0800

    got 30 degree case running after many false starts

--- CHARLES/config2/charles/30_deg_4mps/charles.cpp
+++ CHARLES/config2/charles/30_deg_4mps/charles.cpp
@@ -2,7 +2,8 @@
 #include "../constants.hpp"
 const double uStar = uStar_ref_4mps;
 const double u_scaling = u_ref_4mps;
-const double theta_wind = 0 * M_PI/180;
+const double theta_wind = 30 * M_PI/180;
 const double momentum_scaling_factor = 1.0;
 
 #include "../../../charles_mods/general_mods.hpp"
+
