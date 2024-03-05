b3aed3d8552178e7cad605732a9b57e13ae69abb
Nicholas Bachand <ngbachand@gmail.com>
2024-03-05 10:15:59 -0800

    updating video scripts

--- CHARLES/config2/SherlockVidoesHelper.sh
+++ CHARLES/config2/SherlockVidoesHelper.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 module load system libglvnd/1.2.0
 source ~/.bash_profile
-conda activate pycascade
 
 PARENT_DIR="Cascade/city_block_cfd/CHARLES/config2"
 LOCAL_FOLDER=$(basename "`pwd`")
 
-IMAGE_DIR=$SCRATCH/$PARENT_DIR/$LOCAL_FOLDER/Images/images_rescaled/
+IMAGE_DIR=$SCRATCH/$PARENT_DIR/$LOCAL_FOLDER/Images
 echo "Getting images from $IMAGE_DIR"
-
-cd $SCRATCH/$PARENT_DIR/$LOCAL_FOLDER
 mkdir "./Videos"
 cd "./Videos"
 echo "Saving videos in $(pwd)"
 
 conda activate pycascade
-
