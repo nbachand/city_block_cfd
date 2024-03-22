f31bd6a2afd88135f70885e787be58a355e7095f
Nicholas Bachand <ngbachand@gmail.com>
2024-03-21 17:28:55 -0700

    adding all runs

--- CHARLES/sbatchBulk.sh
+++ CHARLES/sbatchBulk.sh
@@ -4,7 +4,7 @@ bashScript="$1"
 
 # Define an array of numbers
 numbersC2=(21 22 23 24 25 27 28 29 30 33 34 35)
-numbersC3=() #(1 2 3 4)
+numbersC3=(1 2 3 4)
 
 # Iterate through the array
 cd ./config2
