c94e6aef4cf2a3c52d10745c34afffc1ca3f4b79
Nicholas Bachand <ngbachand@gmail.com>
2024-04-19 17:49:16 -0700

    More plots

--- CHARLES/sbatchBulk.sh
+++ CHARLES/sbatchBulk.sh
@@ -3,8 +3,8 @@
 bashScript="$1"
 
 # Define an array of numbers
-numbersC2=(21 22 23 24 25 27 28 29 30 33 34 35)
-numbersC3=(1 2 3 4)
+# numbersC2=(21 22 23 24 25 27 28 29 30 33 34 35)
+numbersC3=(5 6 7 8 9 10 11 12)
 
 # Iterate through the array
 cd ./config2
