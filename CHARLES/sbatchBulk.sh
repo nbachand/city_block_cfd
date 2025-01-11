30c7beb00105b6cedf82b899712efccdc18dba3e
Nicholas Bachand <ngbachand@gmail.com>
2025-01-10 17:45:18 -0800

    running pointcloud probes

--- CHARLES/sbatchBulk.sh
+++ CHARLES/sbatchBulk.sh
@@ -3,8 +3,10 @@
 bashScript="$1"
 
 # Define an array of numbers
-numbersC2=(53 54 55 56)
-numbersC3=(21 22 23 24)
+# numbersC2=(53 54 55 56)
+# numbersC3=(21 22 23 24)
+# numbersC2=(57 58 59 60)
+numbersC3=(25 26 27 28)
 
 # Iterate through the array
 cd ./config2
