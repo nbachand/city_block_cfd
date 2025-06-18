bcfbf09cfc18cef6b78570d96b4cafb583bb90bf
nbachand <42705584+nbachand@users.noreply.github.com>
2025-06-18 16:07:33 -0700

    moving Cd calcs outside of vent update function

--- flowEmulationUtils.py
+++ flowEmulationUtils.py
@@ -133,92 +133,9 @@ def findOptimalP0AndC(rho, flowParams, weight=1, disp=False):
 ##############
 
 
-def getC_ds_AofA(aCorner, aCross, aDual, aSingle, aSinA, aCosA, AofA = 180, roomType="corner"):
-    AofA *= np.pi / 180
-    C_d = aSinA * np.sin(AofA) + aCosA * np.cos(AofA)
-    if roomType == "corner":
-        return C_d + aCorner
-    if roomType == "cross":
-        return C_d + aCross
-    if roomType == "dual":
-        return C_d + aDual
-    if roomType == "single":
-        return C_d + aSingle
-    
-def getC_ds_AofA2(aCorner, aCross, aSingle, aCornerSinA, aCrossSinA, AofA = 180, roomType="corner"):
-    AofA *= np.pi / 180
-    if roomType == "corner":
-        return aCornerSinA * np.sin(AofA) + aCorner
-    if roomType == "cross":
-        return aCrossSinA * np.sin(AofA) + aCross
-    if roomType == "dual":
-        return aCornerSinA * np.sin(AofA) + aCorner
-    if roomType == "single":
-        return aSingle
-
-def getC_ds_AofA3(aCorner, aCross, aDual, aSingle, aSinA, aCosA, sSin2a, aCos2A, AofA = 180, roomType="corner"):
-    AofA *= np.pi / 180
-    C_d = aSinA * np.sin(AofA) + aCosA * np.cos(AofA) + sSin2a * np.sin(2 * AofA) + aCos2A * np.cos(2 * AofA)
-    if roomType == "corner":
-        return C_d + aCorner
-    if roomType == "cross":
-        return C_d + aCross
-    if roomType == "dual":
-        return C_d + aDual
-    if roomType == "single":
-        return C_d + aSingle
-
-def getC_ds_EP(aCorner, aCross, aDual, aSingle, aShear, aNormal, shear=0, normal=0, roomType="corner"):
-    C_d = aShear * shear + aNormal * normal
-    if roomType == "corner":
-        return C_d + aCorner
-    if roomType == "cross":
-        return C_d + aCross
-    if roomType == "dual":
-        return C_d + aDual
-    if roomType == "single":
-        return C_d + aSingle
-
-def getC_ds_All(aCorner, aCross, aDual, aSingle, aSinA, aCosA,  aShear, aNormal, aWS, AofA = 180, shear=0, normal=0, WS=0, roomType="corner"):
-    AofA *= np.pi / 180
-    C_d = aSinA * np.sin(AofA) + aCosA * np.cos(AofA) + aShear * shear + aNormal * normal + aWS * WS
-    if roomType == "corner":
-        return C_d + aCorner
-    if roomType == "cross":
-        return C_d + aCross
-    if roomType == "dual":
-        return C_d + aDual
-    if roomType == "single":
-        return C_d + aSingle
-
-def getC_ds_opening(aX, aZ, aXSinA, aZSinA, aXCosA, aZCosA, AofA=0, openingType="xwindow"):
-    AofA *= np.pi / 180
-    if openingType == "xwindow":
-        return aX + aXSinA * np.sin(AofA) + aXCosA * np.cos(AofA)
-    if openingType == "zwindow":
-        return aZ + aZSinA * np.sin(AofA) + aZCosA * np.cos(AofA)
-
-def getC_ds(params, typeC_d="AofA", AofA=0, shear=0, normal=0, openingType="xwindow", roomType="corner"):
-    if typeC_d == "AofA":
-        return getC_ds_AofA(*params, AofA=AofA, roomType=roomType)
-    if typeC_d == "AofA2":
-        return getC_ds_AofA2(*params, AofA=AofA, roomType=roomType)
-    if typeC_d == "AofA3":
-        return getC_ds_AofA3(*params, AofA=AofA, roomType=roomType)
-    if typeC_d == "EP":
-        return getC_ds_EP(*params, shear=shear, normal=normal, roomType=roomType)
-    if typeC_d == "All":
-        return getC_ds_All(*params, AofA=AofA, shear=shear, normal=normal, WS=1, roomType=roomType)
-    if typeC_d == "opening":
-        return getC_ds_opening(*params, openingType=openingType)
-
-
-
-def update_flow_and_ventilation(flowStatsMI, roomVentilationMI, paramsC_d, useDoors=True, pTypes = {"p-noInt": "p_avg-noInt"}, typeC_d="AofA"):
+def update_flow_and_ventilation(flowStatsMI, roomVentilationMI, useDoors=True, pTypes = {"p-noInt": "p_avg-noInt"}):
     flowStatsMI = flowStatsMI.copy()
     roomVentilationMI = roomVentilationMI.copy()
-    flowStatsMI["cosAofA"] = np.round(np.cos(flowStatsMI["AofA"] * np.pi / 180), 2)
-    flowStatsMI["sinAofA"] = np.round(np.sin(flowStatsMI["AofA"] * np.pi / 180), 2)
 
     for pType in pTypes:
         roomVentilationMI[f"{pType}-success"] = False
@@ -242,16 +159,8 @@ def update_flow_and_ventilation(flowStatsMI, roomVentilationMI, paramsC_d, useDo
         for windowKey in windowKeys:
             for pType, pCol in pTypes.items():
                 flowParams[pType].append(flowStatsMI.loc[(run, windowKey), pCol])
-            C_d = getC_ds(
-                paramsC_d,
-                typeC_d = typeC_d,
-                AofA=flowStatsMI.loc[(run, windowKey),"AofA"], 
-                shear=flowStatsMI.loc[(run, windowKey),"EP_shear"], 
-                normal=flowStatsMI.loc[(run, windowKey),"EP_normal"],
-                openingType = flowStatsMI.loc[(run, windowKey),"openingType"],
-                roomType = row["roomType"],
-                )
-            flowParams["C_d"].append(C_d)
+                
+            flowParams["C_d"].append(flowStatsMI.loc[(run, windowKey), "C_d"])
             flowParams["A"].append(A)
             flowParams["z"].append(flowStatsMI.loc[(run, windowKey), "y"])  # y is vertical in simulation
             flowParams["delT"].append(row["mean-T-room"])
