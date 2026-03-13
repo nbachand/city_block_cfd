db3f901dbfece9293e2f8deedaf86462e92f35b4
nbachand <42705584+nbachand@users.noreply.github.com>
2026-03-13 12:29:18 -0700

    Saving out ASHRAE ventilation rates as csv

--- CpASHRAE.py
+++ CpASHRAE.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pyafn
 from pyafn import rho, Cd, g, beta
+import pandas as pd
 
 angles = np.arange(0, 360, 45)  # Angles from 0 to 360 degrees in 1-degree increments
 
@@ -65,28 +66,45 @@ windows = {
 }
 
 rooms = {
-    "Cross-Ventilated": {
+    "cross": {
         "windows": [
             ("large_face_B", 90),
             ("large_face_B", 270),
         ],
-        "rooms": [[1], [1]],
-        "A": [1, 1]
-    },
-    "Corner": {
-        "windows": [
-            ("small_face_A", 0),
-            ("large_face_B", 90),
+        "window_equivalent":
+        [
+            "zwindow_2-0",
+            "zwindow_2-1"
         ],
         "rooms": [[1], [1]],
         "A": [1, 1]
     },
-    "Dual-Room": {
+    "corner": {
+        "windows": [
+            ("small_face_A", 0),
+            ("large_face_B", 90),
+        ],
+        "window_equivalent":
+        [
+            "xwindow_0-0",
+            "zwindow_1-0"
+        ],
+        "rooms": [[1], [1]],
+        "A": [1, 1]
+    },
+    "dual": {
         "windows": [
             ("small_face_A", 0),
             ("large_face_B", 270),
             ("large_face_AB", 270),
             ("door", 0)
+        ],
+        "window_equivalent":
+        [
+            "xwindow_0-1",
+            "zwindow_0-1",
+            "zwindow_1-1",
+            "door_1-1"
         ],
         "rooms": [[1, 0], [1, 0], [0, 1], [1, -1]],
         "A": [1, 1, 1, 3]
@@ -98,9 +116,13 @@ rooms = {
     # ],
 }
 
-vent_rates = {room_name: [] for room_name in rooms}
+vent_rooms = {room_name: [] for room_name in rooms}
+vent_windows = {}
 
 for room_name, room_dict in rooms.items():
+    for window_equivalent in room_dict["window_equivalent"]:
+        if "door" not in window_equivalent: 
+            vent_windows[window_equivalent] = []
     for wind_angle in angles:
         p_w = []
         for window, angle in room_dict["windows"]:
@@ -129,13 +151,14 @@ for room_name, room_dict in rooms.items():
         print(f"Final objective value: {optResults.fun:.6e}")
 
         flows = pyafn.flowField(p_0s, flowParams)
-        if "Dual" in room_name:
+        if "dual" in room_name:
             flows = flows[0:-1]  # Exclude the door flow for dual-room
-        vent_rates[room_name].append(np.sum(np.abs(flows)) / 2)
+        vent_rooms[room_name].append(np.sum(np.abs(flows)) / 2)
+        for i, flow in enumerate(flows):
+            vent_windows[room_dict["window_equivalent"][i]].append(flow)
 
 fig, axes = plt.subplots(3, 1, subplot_kw=dict(projection="polar"), figsize=(2, 6), dpi=160)
-
-for ax, (room_name, rates) in zip(axes.flat, vent_rates.items()):
+for ax, (room_name, rates) in zip(axes.flat, vent_rooms.items()):
     theta = np.deg2rad(angles)
     # Close the polar curve
     theta_closed = np.append(theta, theta[0])
@@ -147,10 +170,30 @@ for ax, (room_name, rates) in zip(axes.flat, vent_rates.items()):
     ax.set_theta_direction(-1)
     ax.set_xlabel("Total Ventilation Rate (m³/s)")
 
+plt.tight_layout()
+# plt.show()
 
-## Read data
+fig, axes = plt.subplots(1, 7, subplot_kw=dict(projection="polar"), figsize=(6, 2), dpi=160)
+for ax, (window_name, rates) in zip(axes.flat, vent_windows.items()):
+    theta = np.deg2rad(angles)
+    # Close the polar curve
+    theta_closed = np.append(theta, theta[0])
+    rates_closed = np.append(rates, rates[0])
 
-
+    ax.plot(theta_closed, rates_closed)
+    ax.set_title(window_name, pad=15)
+    ax.set_theta_zero_location("W")
+    ax.set_theta_direction(-1)
+    ax.set_xlabel("Total Ventilation Rate (m³/s)")
 
 plt.tight_layout()
-plt.show()
+# plt.show()
+
+dfRooms = pd.DataFrame(vent_rooms)
+dfWindows = pd.DataFrame(vent_windows)
+dfRooms ["roomA"] = angles
+dfWindows ["roomA"] = angles
+dfRooms = dfRooms.melt(id_vars=["roomA"], var_name="roomType", value_name="ventilationRate")
+dfWindows = dfWindows.melt(id_vars=["roomA"], var_name="windowType", value_name="ventilationRate")
+dfRooms.to_csv("roomASHRAE.csv", index=True)
+dfWindows.to_csv("windowASHRAE.csv", index=True)
