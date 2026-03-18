ef29ad77e73f753d00ad0260b2a470817fbd3514
nbachand <42705584+nbachand@users.noreply.github.com>
2026-03-18 15:48:05 -0700

    Updating airflow network to directly work with piecewise function. No erros but havent checked results

--- flowEmulationUtils.py
+++ flowEmulationUtils.py
@@ -19,7 +19,7 @@ def getRoomTemp(row, tempStack=False):
     else:
         return row["mean-T-room"]
 
-def update_flow_and_ventilation(flowStatsMI, roomVentilationMI, useDoors=True, pTypes = {"p-noInt": "p_avg-noInt"}, optTypes = ["optp0", "optp0Cd"], tempStack=False, doorCd=0.6):
+def update_flow_and_ventilation(flowStatsMI, roomVentilationMI, useDoors=True, pTypes = {"p-noInt": "p_avg-noInt"}, optTypes = ["optp0", "optp0Cd"], tempStack=False, doorCd=0.6, checkAnalytical=False):
     flowStatsMI = flowStatsMI.copy()
     roomVentilationMI = roomVentilationMI.copy()
 
@@ -48,6 +48,14 @@ def update_flow_and_ventilation(flowStatsMI, roomVentilationMI, useDoors=True, p
                 flowParams[pType].append(flowStatsMI.loc[(run, windowKey), pCol])
 
             C_d = flowStatsMI.loc[(run, windowKey), "C_d"]
+            if "pRMS" in flowStatsMI.columns:
+                if "pRMS" not in flowParams:
+                    flowParams["pRMS"] = []
+                flowParams["pRMS"].append(flowStatsMI.loc[(run, windowKey), "pRMS"])
+            if "qRMS" in flowStatsMI.columns:
+                if "qRMS" not in flowParams:
+                    flowParams["qRMS"] = []
+                flowParams["qRMS"].append(flowStatsMI.loc[(run, windowKey), "qRMS"])
             flowParams["C_d"].append(C_d)
             flowParams["A"].append(A)
             flowParams["z"].append(flowStatsMI.loc[(run, windowKey), "y"])  # y is vertical in simulation
@@ -75,6 +83,10 @@ def update_flow_and_ventilation(flowStatsMI, roomVentilationMI, useDoors=True, p
             qRooms = np.matmul(np.array(flowParams["rooms"]).T, np.array(flowParams["q"]))
             flowParams["q"].append(np.diff(qRooms)[0])
             flowParams["rooms"].append([1, -1])
+            if "pRMS" in flowParams:
+                flowParams["pRMS"].append([0, 0])
+            if "qRMS" in flowParams:
+                flowParams["qRMS"].append([0, 0])
 
         flowParams = utils.dict_apply(np.array)(flowParams)
 
@@ -91,7 +103,7 @@ def update_flow_and_ventilation(flowStatsMI, roomVentilationMI, useDoors=True, p
                     
             for optType in optTypes:
                 if optType == "optp0":
-                    optResult = pyafn.findOptimalP0(flowParams)
+                    optResult = pyafn.findOptimalP0(flowParams, checkAnalytical=checkAnalytical)
                 elif optType == "optp0Cd":
                     optResult = pyafn.findOptimalP0AndC(flowParams)
                 else:
@@ -106,11 +118,17 @@ def update_flow_and_ventilation(flowStatsMI, roomVentilationMI, useDoors=True, p
                 roomVentilationMI.loc[(run, room), f"{pType}_{optType}-success"] = optResult.success
                 roomVentilationMI.loc[(run, room), f"{pType}_{optType}-fun"] = optResult.fun
                 qs = pyafn.flowField(np.array(p0), flowParamsOpt)
+                Cd = flowParamsOpt["C_d"]
+                Cd_col = f"{pType}_{optType}-C_d"
+                if len(Cd.shape) == 2 and Cd_col not in flowStatsMI.columns:
+                    flowStatsMI[Cd_col] = None
+                    flowStatsMI[Cd_col] = flowStatsMI[Cd_col].astype(object)
+
 
                 for i, windowKey in enumerate(windowKeys):
                     flowStatsMI.loc[(run, windowKey), f"{pType}_{optType}-q_model"] = qs[i]
                     flowStatsMI.loc[(run, windowKey), f"{pType}_{optType}-netq_model"] = abs(qs[i])
-                    flowStatsMI.loc[(run, windowKey), f"{pType}_{optType}-C_d"] = flowParamsOpt["C_d"][i]
+                    flowStatsMI.at[(run, windowKey), f"{pType}_{optType}-C_d"] = Cd[i]
                     flowStatsMI.loc[(run, windowKey), f"{pType}_{optType}-p0"] = np.mean(p0)
                     flowStatsMI.loc[(run, windowKey), f"{pType}_{optType}-success"] = optResult.success
                     flowStatsMI.loc[(run, windowKey), f"p0meas"] = np.mean(p0_meas)
